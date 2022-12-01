bec8db50d3cd029e7c847166f719c235bf7aafc4
Alexey <avkostavkost@gmail.com>
2022-12-01 19:42:23 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,6 @@ public class Main {
         System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
+        System.out.println(8+7);
     }
 }
